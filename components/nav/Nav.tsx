52df51025920fbe37afbaed744f2ed9e46b65a1a
Muhammad Kamran <me@mkamran.us>
2022-09-13 22:00:15 -0700

    A custom ICON I made.

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -1,7 +1,22 @@
+import Logo from "./Logo";
+
 type Props = {};
 
 function Nav({}: Props) {
-  return <div>Nav</div>;
+  return (
+    <nav className="flex flex-row items-center justify-between w-full h-24">
+      <div className="p-1 ml-4 rounded-md cursor-pointer hover:shadow-md">
+        <Logo />
+      </div>
+
+      <ul className="flex mr-4 space-x-2 justify-evenly">
+        <li className="font-mono text-xl">Home</li>
+        <li className="font-mono text-xl">About</li>
+        <li className="font-mono text-xl">Trending</li>
+      </ul>
+      <div></div>
+    </nav>
+  );
 }
 
 export default Nav;
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,4 +9,13 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  animation: {
+    wiggle: 'wiggle 1s ease-in-out infinite',
+  },
+  keyframes: {
+    wiggle: {
+      '0%, 100%': { transform: 'rotate(-3deg)' },
+      '50%': { transform: 'rotate(3deg)' },
+    }
+  }
 }
