f5c49cd25109577bc302334e19228bb8656ba20c
Muhammad Kamran <me@mkamran.us>
2022-09-14 17:27:25 -0700

    Minor nav fixes.

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -1,20 +1,29 @@
 import Logo from "./Logo";
+import { UserCircleIcon } from "@heroicons/react/24/solid";
 
 type Props = {};
 
 function Nav({}: Props) {
   return (
     <nav className="flex flex-row items-center justify-between w-full h-24">
-      <div className="p-1 ml-4 rounded-md cursor-pointer hover:shadow-md">
+      <div className="p-1 ml-12 rounded-md cursor-pointer hover:shadow-md">
         <Logo />
       </div>
 
-      <ul className="flex mr-4 space-x-2 justify-evenly">
-        <li className="font-mono text-xl">Home</li>
-        <li className="font-mono text-xl">About</li>
-        <li className="font-mono text-xl">Trending</li>
+      <ul className="flex mr-4 space-x-4 justify-evenly">
+        <li className="font-mono text-xl transition-all duration-300 border-b-2 cursor-pointer border-b-white hover:border-black">
+          Home
+        </li>
+        <li className="font-mono text-xl transition-all duration-300 border-b-2 cursor-pointer border-b-white hover:border-black">
+          About
+        </li>
+        <li className="font-mono text-xl transition-all duration-300 border-b-2 cursor-pointer border-b-white hover:border-black">
+          Trending
+        </li>
       </ul>
-      <div></div>
+      <div className="mr-12">
+        <UserCircleIcon className="w-16 h-16 text-green-400 rounded-full cursor-pointer hover:shadow-lg" />
+      </div>
     </nav>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,24 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'mono': ['IBM Plex Mono'],
+        'body': ['Inter', 'Arial', 'Helvetica', 'sans-serif']
+      },
+      colors: {
+        "accent": "00FF85"
+      },
+      animation: {
+        wiggle: 'wiggle 250ms ease-in-out infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      }
+    },
   },
   plugins: [],
-  animation: {
-    wiggle: 'wiggle 1s ease-in-out infinite',
-  },
-  keyframes: {
-    wiggle: {
-      '0%, 100%': { transform: 'rotate(-3deg)' },
-      '50%': { transform: 'rotate(3deg)' },
-    }
-  }
 }
